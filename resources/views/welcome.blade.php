2e6c00891a1814af0a6890bea01782babdb530dc
Arti <test@youremail.com>
2017-03-07 16:17:04 -0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Welcome to Missouri Soccer Association----ALive and Kickin!!!</div>
+                <div style="text-align: center;"><img src="images/soccer.jpg" height="300" width="800">></div>
+                <div class="panel-body">
+                    Place some welcome text here for the your Basic Starter Application and maybe a description about intended use.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
